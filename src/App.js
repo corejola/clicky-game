5ef5dc430d8def3fdb781710d628cf0e4a992821
Cedric Orejola <cedricorejola@gmail.com>
2019-08-02 18:05:51 -0700

    working through card & character.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import Nav from "./components/Nav"
+import Card from "./components/Card/Card"
+import Data from "../src/data/characters.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    // this is the state data
+    this.state = {
+      currentScore: 0,
+      topScore: 0,
+      // Data
+    }
+  }
+
+
+
+  render() {
+    const characterData = Data.map(item => <Card key={item.id} props={item} />);
+    // console.log(Data[0].name)
+
+    return (
+      <div>
+        {/* Nav Bar
+        Nav bar will contain the title, button click event & current score & top score, this will require the state to be changed and modified  */}
+        <h1>NAVBAR</h1>
+        {/* <Nav /> */}
+        <hr />
+        <h2>Cards Section</h2>
+        {characterData}
+      </div>
+    )
+  }
 }
-
 export default App;
