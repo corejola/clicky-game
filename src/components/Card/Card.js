cc17b9550031ad8b257ebc4bb47a766ca3ee476e
Cedric Orejola <cedricorejola@gmail.com>
2019-08-06 21:00:22 -0700

    reworked child card as class

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import "./style.css";
 
-function Card(props) {
+class Card extends React.Component {
+    state = {
+        clicked: false
+    }
 
-    // console.log(props.props.image)
-    return (
-        /* onClick={} passed function from parent, App.js */
-        //props.props displays the data from the characters.json information
-        //props.onClick runs the parent function gameScore
-        < div className="card" onClick={props.onClick}>
-            <div className="img-container" id={props.props.id} >
-                <img id={props.props.id} alt={props.props.name} src={props.props.image} isClicked={props.props.clicked} />
-            </div>
-        </div >
-    )
+    render() {
+        // console.log(props.props.image)
+        return (
+            /* onClick={} passed function from parent, App.js */
+            //props.props displays the data from the characters.json information
+            //props.onClick runs the parent function gameScore
+            < div className="card" onClick={this.props.onClick}>
+                <div className="img-container" id={this.props.props.id} >
+                    <img id={this.props.props.id} alt={this.props.props.name} src={this.props.props.image} isClicked={this.state.clicked} />
+                </div>
+            </div >
+        )
+    }
 }
 export default Card;
